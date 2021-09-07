033bc900723a4423e6dbe96cdc46c946bb910eac
pradeep <pradeepseleniumhyd@gmail.com>
2021-09-07 12:26:33 +0530

    latest code

--- src/main/java/com/fb/commons/Commons.java
+++ src/main/java/com/fb/commons/Commons.java
@@ -1,18 +1,93 @@
 package com.fb.commons;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
 public class Commons 
 {
+	
+	public FileInputStream fis;
+	public Properties prop   = new Properties();
+	
+	
 	public void navigateUrl(WebDriver driver,String url)
 	{
 		try
 		{
 		driver.get(url);
+		driver.manage().window().maximize();
 		}catch (Exception e) {
 			System.out.println("browser url is not working" + url + e.getMessage());
 			Assert.fail("browser url is not working");
 		}
 	}
+	
+	
+	public void verifyPageTitle(WebDriver driver,String expectedTitle)
+	{
+		try
+		{
+				String actualTitle=driver.getTitle();
+				System.out.println(actualTitle);
+				if(actualTitle.equalsIgnoreCase(expectedTitle))
+				{
+					System.out.println(actualTitle + " is displayed");
+				}else
+				{
+					System.out.println(actualTitle + "is not displayed");
+					Assert.fail(actualTitle + "is not displayed");
+					
+				}
+		}catch(Exception e)
+		{
+			System.out.println("Verify Page Title is failed " + e.getMessage());
+		}
+	}
+	
+	public void verifyText(WebDriver driver,String oxpath, String expectedValue)
+	{
+		try
+		{
+				String actualValue=driver.findElement(By.xpath(oxpath)).getText();
+				System.out.println(actualValue);
+				if(actualValue.equalsIgnoreCase(expectedValue))
+				{
+					System.out.println(actualValue + " is displayed");
+				}else
+				{
+					System.out.println(actualValue + "is not displayed");
+					Assert.fail(actualValue + "is not displayed");
+					
+				}
+		}catch(Exception e)
+		{
+			System.out.println("Verify Page Title is failed " + e.getMessage());
+		}
+	}
+	
+	
+	
+	public  Properties propertiesLoad(String pathoftheproperties)
+	{
+		try
+		{
+			 fis = new FileInputStream(System.getProperty("user.dir")+pathoftheproperties);
+			
+			prop.load(fis);
+			return prop;
+		}catch(Exception e)
+		{
+			System.out.println("Properties Load issue " + e.getMessage());
+			return null;
+		}
+	}
+	
+	
+	
+	
+	
 }
--- src/test/java/com/fb/login/TC008.java
+++ src/test/java/com/fb/login/TC008.java
@@ -1,9 +1,15 @@
 package com.fb.login;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -12,35 +18,50 @@ import com.fb.commons.OpenBrowser;
 
 public class TC008 extends Commons
 
-	{
+{
 	public WebDriver driver;
+
+
+	@BeforeSuite
+	public  void config() throws Exception
+	{
+		/*
+		 * FileInputStream fis = new
+		 * FileInputStream(System.getProperty("user.dir")+"/Config/config.properties");
+		 * Properties prop = new Properties(); prop.load(fis);
+		 */
+		prop = propertiesLoad("//Config//config.properties");
+		//expProperties = propertiesLoad("//Config//ExpectedValues.properties");
+		
+	}
+	
 	@Parameters("browsername")
 	@BeforeMethod
-	public void browsercod(String browsername) throws Exception
-	{
-		
-		OpenBrowser browser=new OpenBrowser();
-		driver=browser.browserOpenCode(browsername);
-	
-	}
-		@Test
-		public void tc003()
-		{
-			/*try
-			{
-			driver.get("htt//en-gb.facebook.com/");
-			}catch (Exception e) {
-				System.out.println("browser url is not working"+ e.getMessage());
-				Assert.fail("browser url is not working");
-			}*/
-			navigateUrl(driver, "http://en-gb.facebook.com/");
-			
-		}
-		
-		@AfterMethod
-		public void browserclose()
-		{
-			driver.quit();
+	public void browsercod(String browsername) throws Exception {
 
-		}
+		OpenBrowser browser = new OpenBrowser();
+		driver = browser.browserOpenCode(browsername);
+
 	}
+
+	@Test
+		public void tc003() throws Exception
+		{
+			
+			navigateUrl(driver, prop.getProperty("prod_url"));
+			Thread.sleep(8000);
+			System.out.println(prop.getProperty("fb_Title"));
+			verifyPageTitle(driver, prop.getProperty("fb_Title"));	
+			String actualValue=driver.findElement(By.xpath("//h2")).getText();		
+			String expectedValue = prop.getProperty("fb_subheader");
+			Assert.assertEquals(actualValue, expectedValue);
+			verifyText(driver, "//a[contains(text(),'Forgot')]", "Forgotten password?");
+			verifyText(driver, "//button[contains(text(),'Log In')]", "Log In");
+		}
+
+	@AfterMethod
+	public void browserclose() {
+		driver.quit();
+
+	}
+}
